873a15f4c662ae1aeb2817fbedcb0ace730ca9f9
jiahaubai <47979607+jiahaubai@users.noreply.github.com>
2024-10-16 22:03:04 +0800

    update

--- audio_test.py
+++ audio_test.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import pyaudio
 import numpy as np
 
-# 設定 GPIO 引腳
+# 設定 GPIO 
 BUZZER_PIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUZZER_PIN, GPIO.OUT)
@@ -15,7 +15,7 @@ RATE = 44100
 
 p = pyaudio.PyAudio()
 
-# 開啟音訊流
+
 stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -26,13 +26,13 @@ try:
     while True:
         # 讀取音訊數據
         data = stream.read(CHUNK, exception_on_overflow=False)
-        # 將音訊數據轉換為 numpy 陣列
+        
         audio_data = np.frombuffer(data, dtype=np.int16)
         # 計算音量（簡單的振幅檢測）
         volume = np.abs(audio_data).mean()
 
-        # 如果音量超過某個閾值，則啟動蜂鳴器
-        if volume > 1000:  # 根據需要調整這個閾值
+        # 如果有音量時，啟動LED燈
+        if volume > 0:  
             GPIO.output(BUZZER_PIN, GPIO.HIGH)
         else:
             GPIO.output(BUZZER_PIN, GPIO.LOW)
@@ -41,9 +41,7 @@ except KeyboardInterrupt:
     print("Program stopped by user.")
 
 finally:
-    # 清理 GPIO 設置
     GPIO.cleanup()
-    # 停止音訊流
     stream.stop_stream()
     stream.close()
     p.terminate()
